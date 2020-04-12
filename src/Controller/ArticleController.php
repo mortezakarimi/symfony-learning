b5bddca538c2a120e22ca7656aa7539062e1404d
Morteza Karimi <mortezak1373@yahoo.com>
2020-04-12 07:54:58 +0430

    آنچه درباره سیمفونی ۴ آموختم | بخش دوم

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+}
