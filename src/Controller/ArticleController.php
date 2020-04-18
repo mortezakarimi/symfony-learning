729e3e5b47beabd04efa048da2c0b241d67199ac
Morteza Karimi <mortezak1373@yahoo.com>
2020-04-18 11:14:30 +0430

    آنچه درباره سیمفونی ۴ آموختم | بخش سوم

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,11 +5,26 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('OMG! My first page already! WOOO!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
     }
 }
